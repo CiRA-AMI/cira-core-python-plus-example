0431c61217393363618c6bc79c37455041b58f4f
CiRA-AMI <30405116+CiRA-AMI@users.noreply.github.com>
2024-02-28 11:29:37 +0700

    Update cira_core_api_client.py

--- cira_core_api_client/cira_core_api_client.py
+++ cira_core_api_client/cira_core_api_client.py
@@ -6,15 +6,15 @@ import cv2
 import json
 import numpy as np
 
-url = "http://localhost:8082" #you can change server ip here
+url = "http://localhost:8082/api" #you can change server ip here
 
-file = open('img.png', 'rb') 
+file = open('img.jpg', 'rb') 
 
 m = MultipartEncoder(
     fields={
         'service_name': 'deepdetect',  #in example you can change to facerec
         'jsonstr': '{"a": 100}',       #send payload
-        'image': ('img.png', file)
+        'image': ('img.jpg', file)
     }
 )
 
@@ -29,7 +29,7 @@ have_img = len(res.content) > 0
 payload_out = json.loads(json_result['jsonstr'])['payload']
 print(payload_out)
 
-if ok and have_img :
+if have_img :
     img = cv2.imdecode(np.frombuffer(res.content, np.uint8), cv2.IMREAD_COLOR)
     cv2.imshow('img_out', img)
     cv2.waitKey(3000)
